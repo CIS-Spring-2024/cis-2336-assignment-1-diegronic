dbc7debf5baa31aed9670ac59de7b17a7b731c08
diegronic <dieg1997@gmail.com>
2024-03-08 18:08:35 -0600

    I was under the impression that this was due 3/31 as indicated in the syllabus, canva and TA email. Please don't consider late

---
+++ main.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.hamburger-menu');
+    const menu = document.querySelector('.nav-links');
+
+    menuBtn.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
